f38ad643e09e7b9d0e270d6afc063be451e350f3
Davit Menteshashvili <99358958+Menteshex@users.noreply.github.com>
2023-06-29 15:30:51 +0400

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('Hello World!');
 
-streamlit.header('Menu');
-streamlit.text('Fruits');
-streamlit.text('Vegetables');
-streamlit.text('Meat');
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
